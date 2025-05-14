cc29a31d7a53c93249ec122dead6cca78db631d5
LeonardoLobas <leonardolobas2703@gmail.com>
2025-05-14 10:57:53 -0300

    Adiciona página inicial e layout, implementa formulário de login com validação e gerenciamento de cookies

--- src/app/loginPage/page.tsx
+++ src/app/loginPage/page.tsx
@@ -3,55 +3,78 @@ import { z } from "zod";
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { postLoginForm } from "@/request/loginPost";
+import { useCallback } from "react";
+import { useRouter } from "next/navigation";
 
 const createLoginFormSchema = z.object({
     user: z.string().min(1, "user é obrigatorio"),
     password: z.string().min(1, "Senha é obrigatorio"),
-    signIn: z.boolean().optional(),
+    signIn: z.boolean(),
 });
 
 export type createLoginFormData = z.infer<typeof createLoginFormSchema>;
 
 export default function Home() {
     const {
-        register,
         control,
         handleSubmit,
         formState: { errors },
     } = useForm<createLoginFormData>({
         resolver: zodResolver(createLoginFormSchema),
+        defaultValues: { signIn: true },
     });
 
-    async function handleLogin(data: createLoginFormData) {
-        console.log("entrou no kevin");
-        const response = await postLoginForm(data);
-        console.log(response);
-    }
+    const router = useRouter();
+
+    const handleLogin = useCallback(
+        async (data: createLoginFormData) => {
+            console.log("Entrou no kevin", data);
+            const response = await postLoginForm(data);
+
+            if (response instanceof Error) {
+                console.error("Erro ao logar", response.message);
+                return;
+            }
+
+            if (response) {
+                router.push("/home");
+            }
+            console.log(response);
+        },
+        [router]
+    );
 
     return (
-        <div className="grid  items-center justify-items-center bg-green-700 min-h-screen  ">
+        <div className="grid  items-center justify-items-center content-center bg-green-700 min-h-screen  ">
             <form onSubmit={handleSubmit(handleLogin)} className="grid">
-                <label htmlFor="user">user</label>
-                {/* <input
-                    //{...register("user")}
-                    className="bg-amber-50 border-2 border-amber-300"
-                /> */}
+                <label htmlFor="user">User</label>
                 <Controller
                     control={control}
                     name="user"
                     render={({ field: { onChange, value } }) => (
-                        <input onChange={onChange} value={value} />
+                        <input
+                            className="bg-amber-50 border-2 border-amber-300"
+                            onChange={onChange}
+                            value={value}
+                        />
                     )}
                 />
                 {errors.user && <span>{errors.user.message}</span>}
                 <label htmlFor="senha">Senha</label>
-                <input
-                    {...register("password")}
-                    className="bg-amber-50 border-2 border-amber-300"
+                <Controller
+                    control={control}
+                    name="password"
+                    render={({ field: { onChange, value } }) => (
+                        <input
+                            className="bg-amber-50 border-2 border-amber-300"
+                            onChange={onChange}
+                            value={value}
+                        />
+                    )}
                 />
                 {errors.password && <span>{errors.password.message}</span>}
 
-                <button type="submit" className="w-28 h-2 bg-amber-50">
+                <button type="submit" className="w-28 h-6 mt-2.5 bg-amber-50">
                     Submit
                 </button>
             </form>
--- src/request/loginPost.ts
+++ src/request/loginPost.ts
@@ -2,12 +2,37 @@
 
 import { axiosInstance } from "@/api";
 import { createLoginFormData } from "@/app/loginPage/page";
+import { cookies } from "next/headers";
 
-export async function postLoginForm(params: createLoginFormData) {
+export interface IResponseLogin {
+    access_token: string;
+    refresh_token: string;
+}
+
+export async function postLoginForm(
+    params: createLoginFormData
+): Promise<boolean | Error> {
     try {
-        const response = await axiosInstance.post("/auth/login", params);
-        return response;
+        const response = await axiosInstance.post<IResponseLogin>(
+            "/auth/login",
+            params
+        );
+        const cookiesStore = await cookies();
+        cookiesStore.set("access_token", response.data.access_token, {
+            secure: true,
+            httpOnly: true,
+            sameSite: "strict",
+        });
+        cookiesStore.set("refresh_token", response.data.refresh_token, {
+            secure: true,
+            httpOnly: true,
+            sameSite: "strict",
+        });
+
+        console.log(params);
+        console.log(response.data);
+        return true;
     } catch (error) {
-        return error;
+        return error as Error;
     }
 }
